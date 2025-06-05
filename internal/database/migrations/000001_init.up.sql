8e39d302cc94bd2e6ff6592ca0d4e36a9ef409bf
ssvikash_20 <arisivikash@gmail.com>
2025-06-05 15:31:02 +0530

    Create users table migration up and down

---
+++ internal/database/migrations/000001_init.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(320) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    verified BOOLEAN NOT NULL DEFAULT FALSE
+);
