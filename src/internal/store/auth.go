478e217f4b4d231c0f29e59ef06f6e4101fb0542
ssvikash_20 <arisivikash@gmail.com>
2025-06-06 15:57:12 +0530

    Create user

--- src/internal/store/auth.go
+++ src/internal/store/auth.go
@@ -1,11 +1,43 @@
 package store
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthStore struct {
 	db *pgxpool.Pool
 }
 
-func (auth *AuthStore) Test() {
-	return
+func hashedPassword(password string) []byte {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("Failed to hash the password")
+	}
+
+	return hash
+}
+
+func (auth *AuthStore) CreateUser(ctx context.Context, first_name, last_name, email, password string) error {
+	passwordHash := hashedPassword(password)
+
+	sql := `
+		INSERT INTO users(first_name, last_name, email, password_hash) VALUES($1, $2, %3. $4)
+	`
+
+	_, err := auth.db.Exec(ctx, sql,
+		first_name,
+		last_name,
+		email,
+		passwordHash,
+	)
+	if err != nil {
+		log.Println("Cannot create a new User. Failed at inserting the user details into the table.")
+		return err
+	}
+
+	return nil
 }
--- src/internal/store/store.go
+++ src/internal/store/store.go
@@ -1,10 +1,14 @@
 package store
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Storage struct {
 	Auth interface {
-		Test()
+		CreateUser(ctx context.Context, first_name, last_name, email, password string) error
 	}
 }
 
