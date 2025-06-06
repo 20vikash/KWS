49b84866e20b9c18611ca3fcfbfec436df89eb36
ssvikash_20 <arisivikash@gmail.com>
2025-06-06 15:07:13 +0530

    Postgres connection pool

--- src/internal/database/connection/connect.go
+++ src/internal/database/connection/connect.go
@@ -1,1 +1,27 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func GetNewDBConnection() *pgxpool.Pool {
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DBNAME")
+
+	url := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	dbPool, err := pgxpool.New(context.Background(), url)
+	if err != nil {
+		log.Fatal("Cannot create database pool\n")
+	}
+
+	return dbPool
+}
