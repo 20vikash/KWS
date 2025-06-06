56ee41b3f6217732e71d45e83ad194a8dc1c9b7b
ssvikash_20 <arisivikash@gmail.com>
2025-06-06 19:07:31 +0530

    Gmail address validation

--- src/models/users.go
+++ src/models/users.go
@@ -1,9 +1,18 @@
 package models
 
+import "regexp"
+
 type User struct {
 	Email      string
 	First_name string
 	Last_name  string
 	User_name  string
 	Password   string
+}
+
+func (u *User) ValidateEmail() bool {
+	regex := `(?i)^[a-zA-Z0-9._%+\-]+@gmail\.com$`
+
+	re := regexp.MustCompile(regex)
+	return re.MatchString(u.Email)
 }
