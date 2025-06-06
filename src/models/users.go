b37be3f50804b6e007dc31d6a99fe682017532c4
ssvikash_20 <arisivikash@gmail.com>
2025-06-06 19:15:10 +0530

    Validate password

--- src/models/users.go
+++ src/models/users.go
@@ -1,6 +1,9 @@
 package models
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 type User struct {
 	Email      string
@@ -15,4 +18,30 @@ func (u *User) ValidateEmail() bool {
 
 	re := regexp.MustCompile(regex)
 	return re.MatchString(u.Email)
+}
+
+func (u *User) ValidatePassword() bool {
+	if len(u.Password) < 8 {
+		return false
+	}
+
+	hasUpper := false
+	hasLower := false
+	hasNumber := false
+	hasSpecial := false
+
+	for _, c := range u.Password {
+		switch {
+		case 'a' <= c && 'z' >= c:
+			hasLower = true
+		case 'A' <= c && 'Z' >= c:
+			hasUpper = true
+		case '0' <= c && '9' >= c:
+			hasNumber = true
+		case strings.ContainsRune("!@#$%^&*()-_=+[]{}|;:'\",.<>/?", c):
+			hasSpecial = true
+		}
+	}
+
+	return (hasUpper && hasLower && hasNumber && hasSpecial)
 }
