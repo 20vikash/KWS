3330e82568e0c1a5c153d7e5a7c7070a3af262e7
ssvikash_20 <arisivikash@gmail.com>
2025-06-08 20:35:57 +0530

    Get docker connection

--- src/internal/docker/docker.go
+++ src/internal/docker/docker.go
@@ -1,17 +1,44 @@
 package docker
 
-func CreateImageCore() {
+import (
+	"context"
+	"log"
+
+	"github.com/docker/docker/client"
+)
+
+type Docker struct {
+	con *client.Client
+}
+
+func GetConnection() (*client.Client, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Println("Cannot establish connection to the docker daemon")
+		return nil, err
+	}
+
+	_, err = cli.Ping(context.Background())
+	if err != nil {
+		log.Println("Cannot ping the docker daemon")
+		return nil, err
+	}
+
+	return cli, nil
+}
+
+func (d *Docker) CreateImageCore() {
 
 }
 
-func CreateContainer(containerName, volumeName, networkName string) {
+func (d *Docker) CreateContainer(containerName, volumeName, networkName string) {
 
 }
 
-func DeleteContainer(containerName string) {
+func (d *Docker) DeleteContainer(containerName string) {
 
 }
 
-func FindContainerIP(containerName string) {
+func (d *Docker) FindContainerIP(containerName string) {
 
 }
