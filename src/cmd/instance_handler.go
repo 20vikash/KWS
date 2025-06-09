6ce457ec3280cc4c314bfcb916e967f4740409e4
ssvikash_20 <arisivikash@gmail.com>
2025-06-09 19:17:11 +0530

    Add TODO's

--- src/cmd/instance_handler.go
+++ src/cmd/instance_handler.go
@@ -3,13 +3,13 @@ package main
 import "net/http"
 
 func (app *Application) Deploy(w http.ResponseWriter, r *http.Request) {
-
+	//TODO: Pass this request to a message queue, and process it all in the background. SSE all the info.
 }
 
 func (app *Application) StopInstance(w http.ResponseWriter, r *http.Request) {
-
+	//TODO: Same as above
 }
 
 func (app *Application) DeleteInstance(w http.ResponseWriter, r *http.Request) {
-
+	//TODO: Not visible in the UI, CRON jobs will use this endpoint
 }
