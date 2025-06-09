9afa2b6e38a7508914eda85b6d09abcb527dc9bf
ssvikash_20 <arisivikash@gmail.com>
2025-06-09 19:09:55 +0530

    Add all the core stub handlers

--- src/cmd/instance_handler.go
+++ src/cmd/instance_handler.go
@@ -5,3 +5,11 @@ import "net/http"
 func (app *Application) Deploy(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func (app *Application) StopInstance(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) DeleteInstance(w http.ResponseWriter, r *http.Request) {
+
+}
