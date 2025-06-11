862b16882dffefdf2e67d437793b92dfe52fce84
ssvikash_20 <arisivikash@gmail.com>
2025-06-11 14:22:43 +0530

    Publish message to the queue

--- src/internal/store/messageQueue.go
+++ src/internal/store/messageQueue.go
@@ -1,15 +1,18 @@
 package store
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
+	"log"
 
-	"github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type MQ struct {
-	Ch       *amqp091.Channel
-	Queue    *amqp091.Queue
-	Consumer <-chan amqp091.Delivery
+	Ch       *amqp.Channel
+	Queue    *amqp.Queue
+	Consumer <-chan amqp.Delivery
 }
 
 type QueueMessage struct {
@@ -18,8 +21,31 @@ type QueueMessage struct {
 	UserName string
 }
 
-func (mq *MQ) PushMessageInstance(ctx context.Context, message *QueueMessage) {
+func (mq *MQ) PushMessageInstance(ctx context.Context, message *QueueMessage) error {
+	var bin_buf bytes.Buffer
 
+	// Convert the message struct into bytes.
+	err := gob.NewEncoder(&bin_buf).Encode(message)
+	if err != nil {
+		log.Println("Cannot encode the message struct")
+		return err
+	}
+
+	// Publish the message to the queue
+	err = mq.Ch.PublishWithContext(ctx,
+		"",            // exchange
+		mq.Queue.Name, // routing key
+		false,         // mandatory
+		false,         // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(bin_buf.Bytes()),
+		})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (mq *MQ) ConsumeMessageInstance() {
