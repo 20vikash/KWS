987f011449ab97c489b874aa009f55ac319621a3
ssvikash_20 <arisivikash@gmail.com>
2025-06-18 21:12:28 +0530

    Wireguard handler stubs

---
+++ src/cmd/wireguard_handler.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func (app *Application) RegisterDevice(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) RemoveDevice(w http.ResponseWriter, r *http.Response) {
+
+}
