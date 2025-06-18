c2ffc3304cf55d877d8a74f572254ea6021ba6ef
ssvikash_20 <arisivikash@gmail.com>
2025-06-18 21:57:32 +0530

    RegisterDevice handler with TODO

--- src/cmd/wireguard_handler.go
+++ src/cmd/wireguard_handler.go
@@ -1,9 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type RequestBody struct {
+	PublicKey string `json:"public_key"`
+}
 
 func (app *Application) RegisterDevice(w http.ResponseWriter, r *http.Request) {
+	var rb RequestBody
 
+	// Read User ID from the session token
+	uid := app.SessionManager.GetInt(r.Context(), "id")
+
+	// Decode JSON body into struct
+	err := json.NewDecoder(r.Body).Decode(&rb)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	err = app.Wg.AddPeer(r.Context(), uid, rb.PublicKey, app.IpAlloc)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	// Send success response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully added peer"))
+	// TODO: Return a JSON response with the allocated IP
 }
 
 func (app *Application) RemoveDevice(w http.ResponseWriter, r *http.Response) {
