5b877f5ecac0ccadeb10567821e2ef7a7bbcb936
ssvikash_20 <arisivikash@gmail.com>
2025-06-18 13:33:14 +0530

    Set max conns to 20 for pg

--- src/internal/database/connection/connect_pg.go
+++ src/internal/database/connection/connect_pg.go
@@ -19,6 +19,14 @@ type Pg struct {
 func (p *Pg) GetNewDBConnection() *pgxpool.Pool {
 	url := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", p.User, p.Password, p.Host, p.Port, p.Name)
 
+	// Set max parallel connections to 20
+	config, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		log.Fatal("Failed to parse pg config")
+	}
+
+	config.MaxConns = 20
+
 	dbPool, err := pgxpool.New(context.Background(), url)
 	if err != nil {
 		log.Fatal("Cannot create database pool\n")
