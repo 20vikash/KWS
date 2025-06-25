38feed0254da99327fb1d5bdbc8dff496fa362a7
ssvikash_20 <arisivikash@gmail.com>
2025-06-25 19:19:09 +0530

    pg service handler stubs

---
+++ src/cmd/pg_service_handler.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *Application) CreatePGUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) CreatePgDatabase(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) RemovePgUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) RemovePgDatabase(w http.ResponseWriter, r *http.Request) {
+
+}
