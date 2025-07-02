be08911c50f785c594e1fcc9e2ae3e072a5098fe
ssvikash_20 <arisivikash@gmail.com>
2025-07-02 13:40:33 +0530

    Code server nginx code

--- src/internal/nginx/add_template.go
+++ src/internal/nginx/add_template.go
@@ -1,1 +1,58 @@
 package nginx
+
+import (
+	"fmt"
+	"kws/kws/internal/docker"
+	"log"
+	"os"
+	"text/template"
+)
+
+type Nginx struct {
+	Docker *docker.Docker
+}
+
+type Template struct {
+	Domain string
+	IP     string
+	Port   string
+}
+
+const nginxTemplate = `
+server {
+    listen 80;
+    server_name {{ .Domain }}.kwscloud.in;
+
+    location / {
+        proxy_pass http://{{ .IP }}:{{ .Port }};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+`
+
+func (t *Template) AddCodeServer() error {
+	tmpl, err := template.New("nginx").Parse(nginxTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	// Path to write the new Nginx config file
+	filePath := fmt.Sprintf("/etc/nginx/conf.d/%s.conf", t.Domain)
+
+	// Create and write to the file
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Println("Cannot create file nginx conf")
+		return err
+	}
+	defer f.Close()
+
+	if err := tmpl.Execute(f, t); err != nil {
+		log.Println("nginx templating failed")
+		return err
+	}
+
+	return nil
+}
