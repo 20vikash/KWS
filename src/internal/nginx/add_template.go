7be8b6e3bf99a918cf350025ea2ad3b690ae2f78
ssvikash_20 <arisivikash@gmail.com>
2025-07-05 11:40:41 +0530

    Add Cloudflare SSL

--- src/internal/nginx/add_template.go
+++ src/internal/nginx/add_template.go
@@ -17,6 +17,18 @@ const nginxTemplate = `
 server {
     listen 80;
     server_name {{ .Domain }}.kwscloud.in;
+    return 301 https://$host$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name {{ .Domain }}.kwscloud.in;
+
+    ssl_certificate     /etc/letsencrypt/live/kwscloud.in-0001/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/kwscloud.in-0001/privkey.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers HIGH:!aNULL:!MD5;
 
     location / {
         proxy_pass http://{{ .IP }}:{{ .Port }};
@@ -27,6 +39,8 @@ server {
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        proxy_buffering off;
     }
 }
 `
