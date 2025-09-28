6a0db50595c28862550141449383518f8406b391
ssvikash_20 <arisivikash@gmail.com>
2025-09-28 17:58:53 +0530

    Add name to tunnels model

--- src/models/tunnels.go
+++ src/models/tunnels.go
@@ -1,13 +1,15 @@
 package models
 
 type Tunnels struct {
+	Name     string
 	UID      int
 	Domain   string
 	IsCustom bool
 }
 
-func CreateTunnel(uid int, domain string, isCustom bool) *Tunnels {
+func CreateTunnel(uid int, domain string, isCustom bool, name string) *Tunnels {
 	return &Tunnels{
+		Name:     name,
 		UID:      uid,
 		Domain:   domain,
 		IsCustom: isCustom,
