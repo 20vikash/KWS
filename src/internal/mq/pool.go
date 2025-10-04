e75bc5b80f578e7a6d10d6875708757a97e12ac8
ssvikash_20 <arisivikash@gmail.com>
2025-10-04 18:06:07 +0530

    Populate chanpool

--- src/internal/mq/pool.go
+++ src/internal/mq/pool.go
@@ -4,8 +4,19 @@ import amqp "github.com/rabbitmq/amqp091-go"
 
 var chanPool chan *amqp.Channel
 
-func CreateChannelPool(size int) {
+func CreateChannelPool(size int, mqCon *amqp.Connection) error {
 	chanPool = make(chan *amqp.Channel, size)
+
+	for range size {
+		ch, err := mqCon.Channel()
+		if err != nil {
+			return err
+		}
+
+		chanPool <- ch
+	}
+
+	return nil
 }
 
 func PushChannel(ch *amqp.Channel) {
