4816c1c5e447d7a05637ed0d9167f2fd116d8372
jack-san-145 <santhosh34848550@gmail.com>
2026-07-11 06:38:16 +0000

    feat(routes): register domain result route

--- src/cmd/routes.go
+++ src/cmd/routes.go
@@ -46,6 +46,7 @@ func NewRouter(app *Application) http.Handler {
 		protected.Post("/stopresult", app.StopResult)
 		protected.Post("/killresut", app.KillResult)
 		protected.Post("/adddomain", app.AddUserDomain)
+		protected.Get("/domainresult", app.DomainResult)
 		protected.Post("/removedomain", app.RemoveUserDomain)
 		protected.Route("/kws_services", func(r chi.Router) {
 			r.Get("/", app.RenderServicesPage)
